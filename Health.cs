89891056b060418682c6059fba8bb09487ac4657
Horsey4 <62522547+Horsey4@users.noreply.github.com>
2020-12-28 15:58:17 -0600

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -16,7 +16,7 @@ namespace HealthMod
         public override string ID => "Health";
         public override string Name => "Health";
         public override string Author => "Horsey4";
-        public override string Version => "1.1.0";
+        public override string Version => "1.1.1";
         public override bool SecondPass => true;
         public string saveFile => $@"{ModLoader.GetModConfigFolder(this)}\save.txt";
         public FsmFloat drunk => FsmVariables.GlobalVariables.FindFsmFloat("PlayerDrunk");
@@ -30,7 +30,8 @@ namespace HealthMod
         public ConfigurableJoint vehiJoint;
         public GameObject death;
         public VignetteAndChromaticAberration damageEffect;
-        public AudioClip[] hitSfx;
+        public List<FsmFloat> deathSpeeds = new List<FsmFloat>();
+        public AudioClip[] hitSfx = new AudioClip[8];
         public Material hudMat;
         public Transform HUD;
         public Transform hpBar;
@@ -46,7 +47,6 @@ namespace HealthMod
             FsmVariables.GlobalVariables.FindFsmFloat("PlayerStress"),
             FsmVariables.GlobalVariables.FindFsmFloat("PlayerUrine")
         };
-        public FsmFloat[] deathSpeeds;
         public FsmFloat wasp;
         public float hp;
         public float crashMulti;
@@ -60,35 +60,22 @@ namespace HealthMod
         bool mode;
 
         /* Changelogs
-         * Fixed damage with crashing and bees after you already have died
-         * Fixed player cars damaging you even if you arent in them
-         * Fixed AI crashes not damaginy you at all
-         * Fixed OnSave() error not showing in menu
-         * Added damage from getting hit by lightning
-         * Added damage from drowning while drunk
+         * Fixed another crash damage bug I missed
+         * Fixed an error when the house is burnt down
+         * Fixed blur not going away after death
+         * Minor optimizations
          */
 
-        public override void OnNewGame()
-        {
-            try { File.Delete(saveFile); }
-            catch (Exception e)
-            {
-                error($"Error resetting\n{e.Message}");
-                log($"Error resetting\n{e}");
-            }
-        }
+        public override void OnNewGame() => File.Delete(saveFile);
 
-        public override void OnSave()
-        {
-            File.WriteAllText(saveFile, Convert.ToBase64String(Encoding.UTF8.GetBytes
-            (
-                string.Join(",", new string[]
-                {
-                    hp.ToString(),
-                    poisonCounter.ToString()
-                })
-            )));
-        }
+        public override void OnSave() => File.WriteAllText(saveFile, Convert.ToBase64String(Encoding.UTF8.GetBytes
+        (
+            string.Join(",", new string[]
+            {
+                hp.ToString(),
+                poisonCounter.ToString()
+            })
+        )));
 
         public override void ModSettings()
         {
@@ -96,7 +83,6 @@ namespace HealthMod
             crashHpLoss = new Settings("crashHpLoss", "Crash Damage", true, updateSettings);
             difficulty = new Settings("difficulty", "Difficulty Multiplier (x)", 1f, updateSettings);
             minCrashSpeed = new Settings("minCrashSpeed", "Minimum Crash Speed (km/h)", 20, updateSettings);
-            updateSettings();
 
             Settings.AddText(this, "Vanilla mode cannot be toggled mid-game");
             Settings.AddCheckBox(this, vanillaMode);
@@ -146,20 +132,24 @@ namespace HealthMod
                 // FSM setup
                 var actions = waspFsm.FsmStates.FirstOrDefault(x => x.Name == "Allergy").Actions;
                 var audio = GameObject.Find("MasterAudio/PlayerMisc").transform;
+                var room = GameObject.Find("YARD/Building/LIVINGROOM");
                 actions[1].Enabled = false; // Disable vignetting for wasp stings
                 actions[2].Enabled = false;
                 actions[3].Enabled = false;
                 wiringFsm = GameObject.Find("SATSUMA(557kg, 248)/Wiring").GetComponents<PlayMakerFSM>().FirstOrDefault(x => x.FsmName == "Shock");
                 wiringFsm.FsmStates.FirstOrDefault(x => x.Name == "Random").Actions[0].Enabled = false;
-                callFsm = GameObject.Find("YARD").transform.Find("Building/LIVINGROOM/Telephone/Logic/Ring").GetComponent<PlayMakerFSM>();
-                callFsm.gameObject.SetActive(true);
-                if (!callFsm.transform.parent.gameObject.activeSelf)
+                if (room)
                 {
-                    callFsm.transform.parent.gameObject.SetActive(true);
-                    callFsm.transform.parent.gameObject.SetActive(false);
+                    callFsm = room.transform.Find("Telephone/Logic/Ring").GetComponent<PlayMakerFSM>();
+                    callFsm.gameObject.SetActive(true);
+                    if (!callFsm.transform.parent.gameObject.activeSelf)
+                    {
+                        callFsm.transform.parent.gameObject.SetActive(true);
+                        callFsm.transform.parent.gameObject.SetActive(false);
+                    }
+                    callFsm.gameObject.SetActive(false);
+                    callFsm.FsmStates.FirstOrDefault(x => x.Name == "Random").Actions[0].Enabled = false;
                 }
-                callFsm.gameObject.SetActive(false);
-                callFsm.FsmStates.FirstOrDefault(x => x.Name == "Random").Actions[0].Enabled = false;
                 swimFsm = player.GetComponents<PlayMakerFSM>().FirstOrDefault(x => x.FsmName == "Swim");
                 var state = swimFsm.FsmStates.FirstOrDefault(x => x.Name == "Randomize");
                 state.Transitions[1].FsmEvent = swimFsm.FsmEvents.FirstOrDefault(x => x.Name == "SWIM");
@@ -174,10 +164,8 @@ namespace HealthMod
                 GameObject.Find("MAP").transform.Find("CloudSystem/Clouds/Thunder/GroundStrike").gameObject.AddComponent<LightningListener>().mod = this;
 
                 // Damage setup
-                var hitList = new List<AudioClip>();
                 for (var i = 13; i < 21; i++)
-                    hitList.Add(audio.GetChild(i).GetComponent<AudioSource>().clip); // Get damage sfx
-                hitSfx = hitList.ToArray();
+                    hitSfx[i - 13] = audio.GetChild(i).GetComponent<AudioSource>().clip; // Get damage sfx
                 damageEffect.blur = Mathf.Infinity;
                 damageEffect.blurSpread = 0;
             }
@@ -212,7 +200,6 @@ namespace HealthMod
             HUD.Find("Jailtime").localPosition = new Vector3(-11.5f, 6 - offset);
 
             // Other setup
-            var deathSpeedList = new List<FsmFloat>();
             var cars = Resources.FindObjectsOfTypeAll<Drivetrain>();
             var colls = Resources.FindObjectsOfTypeAll<Collider>();
             var helmetOn = FsmVariables.GlobalVariables.FindFsmBool("PlayerHelmet");
@@ -224,7 +211,7 @@ namespace HealthMod
                 if (cars[i].transform.parent != null && fsms.Length > 0)
                 {
                     for (var x = 0; x < fsms.Length; x++)
-                        if (fsms[x].FsmName.Contains("Throttle")) deathSpeedList.Add(fsms[x].FsmVariables.FindFsmFloat("DeathSpeedMPS"));
+                        if (fsms[x].FsmName.Contains("Throttle")) deathSpeeds.Add(fsms[x].FsmVariables.FindFsmFloat("DeathSpeedMPS"));
                 }
                 else
                 {
@@ -239,7 +226,6 @@ namespace HealthMod
                     (fsm.FsmStates[1].Actions[3] as SetProperty).everyFrame = false;
                 } // Disable setting of joint strength everyframe
             }
-            deathSpeeds = deathSpeedList.ToArray();
             for (var i = 0; i < colls.Length; i++)
                 if (colls[i].name == "SleepTrigger") colls[i].gameObject.AddComponent<SleepListener>().mod = this;
             updateSettings();
@@ -247,6 +233,11 @@ namespace HealthMod
 
         public override void FixedUpdate()
         {
+            // De-blur
+            if (!mode && damageEffect.blurSpread > 0)
+                damageEffect.blurSpread -= 0.1f;
+            else damageEffect.enabled = false;
+
             if (death.activeSelf)
             {
                 hp = 100;
@@ -307,7 +298,7 @@ namespace HealthMod
                 wiringFsm.SendEvent("OFF");
                 if (damage(50, "Electrocute", 0.5f)) kill("Electrocute");
             }
-            if (callFsm.ActiveStateName == "Random")
+            if (callFsm && callFsm.ActiveStateName == "Random")
             {
                 callFsm.SendEvent("SURVIVE");
                 if (damage(50, "PhoneThunder", 0.5f)) kill("PhoneThunder");
@@ -333,23 +324,20 @@ namespace HealthMod
                 if (diff > 0) editHp(diff, "Eat");
                 pHunger = stats[1].Value;
             }
-
-            // De-blur
-            if (damageEffect.blurSpread > 0)
-                damageEffect.blurSpread -= 0.1f;
-            else damageEffect.enabled = false;
         }
 
         void updateSettings()
         {
+            log("Settings updated:");
+            log($"crashHpLoss = {crashHpLoss.Value}");
             difficultyMulti = Mathf.Clamp(Convert.ToSingle(difficulty.Value), 0.5f, 3);
+            log($"difficultyMulti = {difficultyMulti}");
             crashMin = Mathf.Clamp(Convert.ToSingle(minCrashSpeed.Value), 10, 30) * 5 / 18;
+            log($"crashMin = {crashMin}");
 
-            if (deathSpeeds != null && !mode) for (var i = 0; i < deathSpeeds.Length; i++)
+            if (deathSpeeds != null && !mode) for (var i = 0; i < deathSpeeds.Count; i++)
                 deathSpeeds[i].Value = (bool)crashHpLoss.Value ? Mathf.Infinity : 5;
         }
-
-        void error(object str) => ModConsole.Error($"[{ID}] {str}");
 
         void log(object str) => Console.WriteLine($"[{ID}] {str}");
 
@@ -371,7 +359,6 @@ namespace HealthMod
             damageEffect.enabled = true;
             damageEffect.blurSpread += val / 10;
             AudioSource.PlayClipAtPoint(hitSfx[UnityEngine.Random.Range(0, hitSfx.Length - 1)], player.transform.position);
-            if (reason == "Crash") crashCooldown = val;
 
             log($"Blurred because {reason}");
             return damageMulti == 0 || editHp(-val * damageMulti, reason);
